522f95f5e14c3eb77e5f1ce039357f97b8590cf3
이재훈 <kllk1212@naver.com>
2023-08-29 23:54:29 +0900

    fix : 공용컴포넌트 매개변수(이벤트) 추가
    CustomButton : onClick  매개변수 추가
    CustomCalendar : onChange 매개변수 추가
    CustomInput : onChange 매개변수 추가
    CustomRadio : onClick 매개변수 추가
    CustomSelect : onChange 매개변수 추가

--- Front-End/src/components/Contents/CustomSelect.js
+++ Front-End/src/components/Contents/CustomSelect.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-function CustomSelect({ id, className, options, defaultValue }) {
+function CustomSelect({
+  id,
+  className,
+  options,
+  defaultValue,
+  width,
+  onChange,
+}) {
   return (
     <div className={`searchBarBox ${className}`}>
-      <select id={id} className="selectBox" defaultValue={defaultValue}>
+      <select
+        id={id}
+        className="selectBox"
+        defaultValue={defaultValue}
+        style={{ width: `${width}px` }}
+        onChange={onChange}
+      >
         {options.map((option, index) => (
           <option key={index} value={option.value}>
             {option.label}
