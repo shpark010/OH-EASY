2113523a48a0b62bb3ab376dc759b0021847cb11
shpark010 <parksungh12@naver.com>
2023-09-04 18:40:59 +0900

    feat : multi-proxy설정 및 로그인 데이터 cookie 저장
    1. 서버 분리에 따른 multi-proxy 설정
    2. 로그인 성공 데이터 localStorage에서 cookie에 저장되도록 변경

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppContent from "./containers/AppContent";
 import Head from "./components/Head/Head.js";
+import { CookiesProvider } from "react-cookie";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Head />
-        <AppContent />
-      </Router>
+      <React.StrictMode>
+        <CookiesProvider>
+          <Router>
+            <Head />
+            <AppContent />
+          </Router>
+        </CookiesProvider>
+      </React.StrictMode>
     );
   }
 }
--- Front-End/src/containers/PrivateRoute.js
+++ Front-End/src/containers/PrivateRoute.js
@@ -1,8 +1,9 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { setCookie, getCookie, removeCookie } from "../containers/Cookie";
 
 function PrivateRoute() {
-  const token = localStorage.getItem("token");
-
+  //const token = localStorage.getItem("token");
+  const token = getCookie("loginInfo");
   // 토큰이 있으면 요청한 페이지로 이동
   if (token) {
     return <Outlet />;
