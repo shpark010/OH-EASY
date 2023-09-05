90e0e3a1f0c909ec4fccf1bbf3b7874fc18cbb24
shpark010 <parksungh12@naver.com>
2023-09-05 10:36:39 +0900

    fix : header  css 재수정
    상단 고정 해제

--- Front-End/src/components/Header/Header.js
+++ Front-End/src/components/Header/Header.js
@@ -10,11 +10,7 @@ import styled from "styled-components";
 class Header extends Component {
   render() {
     return (
-      <header
-        id="header"
-        className="header"
-        style={{ position: "fixed", top: "0", backgroundColor: "white" }}
-      >
+      <header id="header" className="header">
         <div className="wrapper">
           <Logo />
           <nav id="headerNav" className="nav headerNav">
