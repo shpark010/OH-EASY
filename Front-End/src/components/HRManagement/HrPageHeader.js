24633417b6b84990d8e8cd36c51c1ba1e7fd871d
이재훈 <kllk1212@naver.com>
2023-09-06 17:02:24 +0900

    feat : ApiRequest 생성
    api 요청 기능 컴포넌트 생성

--- Front-End/src/components/HRManagement/HrPageHeader.js
+++ Front-End/src/components/HRManagement/HrPageHeader.js
@@ -7,8 +7,46 @@ import Setting from "../../images/pages/common/setting.png";
 import Calc from "../../images/pages/common/calc.png";
 import Print from "../../images/pages/common/print.png";
 import Delete from "../../images/pages/common/delete.png";
+import useApiRequest from "../Services/ApiRequest";
 
-const HrPageHeader = ({ onFetchEmpData }) => {
+const HrPageHeader = ({ checkedRows, setEmpList }) => {
+  const apiRequest = useApiRequest();
+
+  // 체크된 사원들을 가져와서 db에서 삭제
+  const handleSendCheckedCdEmpListDelete = async () => {
+    try {
+      const responseData = await apiRequest({
+        method: "POST",
+        url: "/api2/hr/HrEmpDataDelete",
+        data: {
+          selectedEmpCodes: checkedRows,
+        },
+      });
+      if (responseData === 1) {
+        // 삭제성공
+        alert("삭제성공");
+      } else {
+        // 삭제 실패
+        alert("삭제실패");
+      }
+    } catch (error) {
+      console.error("api 요청 실패:", error);
+    }
+  };
+
+  // 사원정보 API 요청 후 사원정보 setEmpList
+  const handleGetEmpList = async () => {
+    try {
+      const responseData = await apiRequest({
+        method: "GET",
+        url: "/api2/hr/getAllEmpList",
+      });
+      setEmpList(responseData);
+    } catch (error) {
+      console.error("Failed to fetch emp data:", error);
+    }
+  };
+
   return (
     <div className="pageHeader">
       <div className="innerBox fxSpace">
@@ -17,7 +55,7 @@ const HrPageHeader = ({ onFetchEmpData }) => {
           <div className="btnWrapper textBtnWrap">
             <PageHeaderTextButton
               text="사원불러오기"
-              onClick={onFetchEmpData}
+              onClick={handleGetEmpList}
             />
           </div>
           <div className="iconBtnWrap">
@@ -30,6 +68,7 @@ const HrPageHeader = ({ onFetchEmpData }) => {
               btnName="delete"
               imageSrc={Delete}
               altText="삭제"
+              onClick={handleSendCheckedCdEmpListDelete}
             />
             <PageHeaderIconButton
               btnName="calc"
---
+++ Front-End/src/components/Services/ApiRequest.js
@@ -0,0 +1,73 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useApiRequest = () => {
+  // React Router의 navigate 함수를 사용하여 페이지 이동을 수행
+  const navigate = useNavigate();
+
+  // 주어진 쿠키 이름(cname)을 기반으로 해당 쿠키의 값을 반환
+  const getCookie = (cname) => {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === " ") {
+        c = c.substring(1);
+      }
+      // 해당 이름의 쿠키를 찾았다면 값을 반환
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    // 쿠키를 찾지 못한 경우 빈 문자열을 반환
+    return "";
+  };
+
+  // API 요청을 수행하는 함수
+  const apiRequest = async (props) => {
+    const { method, url, data } = props;
+
+    // "loginInfo"라는 이름의 쿠키 값
+    const loginInfo = getCookie("loginInfo");
+
+    // loginInfo 쿠키가 없는 경우 사용자를 로그인 페이지로 리디렉션
+    if (!loginInfo) {
+      alert("유효하지 않은 요청입니다");
+      navigate("/login");
+      return;
+    }
+
+    // loginInfo 쿠키에서 토큰 정보를 추출
+    const token = loginInfo.split(".")[1];
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    // 토큰이 있을 경우, 헤더에 추가
+    if (token) {
+      headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    // 실제로 API를 호출하는 부분
+    try {
+      const response = await axios({
+        method,
+        url,
+        headers,
+        data: JSON.stringify(data),
+      });
+      return response.data;
+    } catch (error) {
+      // API 요청 중 오류가 발생한 경우 콘솔에 오류를 기록하고, 사용자를 로그인 페이지로 리디렉션
+      console.error("API request failed:", error);
+      navigate("/login");
+      return null;
+    }
+  };
+
+  // apiRequest 함수를 반환
+  return apiRequest;
+};
+
+export default useApiRequest;
