09412a8f3a947c1402652195206a2ab3aff3e5c5
이재훈 <kllk1212@naver.com>
2023-09-06 17:53:10 +0900

    feat :  HrController 삭제 메서드 추가
    삭제 메서드 추가

--- Back-End/OhEasy-Data-API/src/main/java/kr/or/oheasy/hrm/controller/HrController.java
+++ Back-End/OhEasy-Data-API/src/main/java/kr/or/oheasy/hrm/controller/HrController.java
@@ -6,14 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.or.oheasy.hrm.service.HrService;
 import kr.or.oheasy.vo.HrEmpMstVO;
 
 @RestController
-@RequestMapping("/hr")
+@RequestMapping("/api2/hr")
 public class HrController {
 	
 	@Autowired
@@ -26,5 +29,15 @@ public class HrController {
 		
 		return new ResponseEntity<>(result,HttpStatus.OK);
 	}
+	
+	@PostMapping("HrEmpDataDelete")
+	public ResponseEntity<?> empDataDelete(@RequestBody String data){
+		
+		System.out.println(data);
+		
+		return new ResponseEntity<>(1,HttpStatus.OK);
+	}
+	
+	
 	
 }
