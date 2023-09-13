76b0bfb4278d2ed61845e9b79e6a29671a350910
이재훈 <kllk1212@naver.com>
2023-09-13 21:09:50 +0900

    feat : hr 페이지 수정 , apiRequest 수정
    hr 페이지 조건 검색 추가 ,apiRequest 토큰 추출 로직 수정

--- Front-End/src/components/HRManagement/HrSearchBar.js
+++ Front-End/src/components/HRManagement/HrSearchBar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import SearchBarBox from "../SearchBar/SearchBarBox";
 import SearchSubmitButton from "../SearchBar/SearchSubmitButton";
+import useApiRequest from "../Services/ApiRequest";
+const HrSearchBar = ({ conditions, setConditions, setEmpList }) => {
+  const apiRequest = useApiRequest();
 
-const HrSearchBar = () => {
+  const handleSelectChange = (e, name) => {
+    setConditions((prevState) => ({ ...prevState, [name]: e.target.value }));
+  };
+
+  const handleSearchBtnClick = async () => {
+    console.log("클릭 시점");
+    try {
+      const responseData = await apiRequest({
+        method: "GET",
+        url: `/api2/hr/getConditionalEmpList?category=${conditions.category}&sort=${conditions.sort}`,
+      });
+      console.log(responseData);
+      setEmpList(responseData);
+    } catch (error) {
+      console.error("api 요청 실패:", error);
+    }
+  };
+
   return (
     <div className="searchBar">
       <div className="innerBox fxSpace">
@@ -11,26 +31,29 @@ const HrSearchBar = () => {
             label="조회구분"
             id="hr-category"
             options={[
-              { value: "0", label: "0. 재직자+당해년도 퇴사자" },
-              { value: "1", label: "1. 재직자+당해년도 퇴사자" },
-              { value: "2", label: "2. 작년퇴사자+당해년도 퇴사자" },
-              { value: "3", label: "4. 박성환" },
+              { value: 0, label: "0. 재직자" },
+              { value: 1, label: "1. 퇴사자" },
+              { value: 2, label: "2. 재직자+당해년도 퇴사자" },
+              { value: 3, label: "3. 작년 퇴사자+당해년도 퇴사자" },
             ]}
-            defaultValue="1"
+            value={conditions.category}
+            placeholder="선택"
+            onChange={(e) => handleSelectChange(e, "category")}
           />
           <SearchBarBox
             label="정렬"
             id="hr-order"
             options={[
-              { value: "0", label: "0. 코드순" },
-              { value: "1", label: "1. 이름순" },
-              { value: "1", label: "1. 입사순" },
+              { value: 0, label: "0. 코드순" },
+              { value: 1, label: "1. 이름순" },
+              { value: 2, label: "2. 입사순" },
             ]}
-            defaultValue="0"
+            value={conditions.sort}
+            onChange={(e) => handleSelectChange(e, "sort")}
           />
         </div>
         <div className="btnWrapper">
-          <SearchSubmitButton text="조회" />
+          <SearchSubmitButton text="조회" onClick={handleSearchBtnClick} />
         </div>
       </div>
     </div>
